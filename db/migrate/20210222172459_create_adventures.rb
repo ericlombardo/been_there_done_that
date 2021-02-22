565df37f00dcb88d4765843439a58f9d50161665
Eric <eric.m.lombardo@gmail.com>
2021-02-22 13:14:21 -0500

    Create Adventure class model and table
    - Create Adventure model class
    - Create adventures table with following columns
      * title     * rating        * go_again?       * start_date
      * end_date  * miles_covered * companions      * favorite_memory
      * weather   * summary       * transportation  *food
      * notes     * misc

---
+++ db/migrate/20210222172459_create_adventures.rb
@@ -0,0 +1,22 @@
+class CreateAdventures < ActiveRecord::Migration[5.2]
+  def change
+    create_table :adventures do |t|
+      t.string :title
+      t.integer :rating
+      t.boolean :go_again?
+      t.string :start_date
+      t.string :end_date
+      t.float :miles_covered
+      t.string :companions
+      t.text :favorite_memory
+      t.string :weather
+      t.text :summary
+      t.string :transportation
+      t.string :food
+      t.text :notes
+      t.text :misc
+
+      t.integer :user_id
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_171459) do
+ActiveRecord::Schema.define(version: 2021_02_22_172459) do
+
+  create_table "adventures", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.boolean "go_again?"
+    t.string "start_date"
+    t.string "end_date"
+    t.float "miles_covered"
+    t.string "companions"
+    t.text "favorite_memory"
+    t.string "weather"
+    t.text "summary"
+    t.string "transportation"
+    t.string "food"
+    t.text "notes"
+    t.text "misc"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
