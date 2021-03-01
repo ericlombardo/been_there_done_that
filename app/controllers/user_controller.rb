1f1dabb8bd0fab63fdc5c0c78c507b2fd2d828b8
Eric <eric.m.lombardo@gmail.com>
2021-03-01 10:05:36 -0500

    Add handling for not logged in user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -24,8 +24,8 @@ class UserController < ApplicationController
   end
 
   get "/users/:id/adventures" do
-    binding.pry
-    @my_adventures = find_user.adventures
+    block_not_logged_in
+    @adventures = find_user.adventures
 
     erb :"users/show_adventures"
   end
