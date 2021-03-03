9326bce8a7b6950ca4251a1f0f13f39afb6313fd
Eric <eric.m.lombardo@gmail.com>
2021-03-03 01:02:49 -0500

    Add astricks to edit view to show required fields

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -3,4 +3,7 @@ class Adventure < ActiveRecord::Base
   has_many :adventure_state_activities
   has_many :activities, through: :adventure_state_activities
   has_many :states, through: :adventure_state_activities
+
+  # validations
+  
 end
