436ccb70a252390073cec0ad18d874e86e5c50f2
Eric <eric.m.lombardo@gmail.com>
2021-03-03 16:12:43 -0500

    Collect user states, activities and top 3 for each

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,13 +3,16 @@ class UserController < ApplicationController
   get "/users/:id" do
     if logged_in?
       @user = User.find(params[:id])
+      @states = @user.adventures.collect {|a| a.states.uniq}.flatten    # get all states for that user, not doubled up
+      @activities = @user.adventures.collect{|a| a.activities}.flatten  # get all activities for that user, not doubled up
+    
+      # loops through states, create new hash set value to (0) if no value, create key/value for state.name/count
+      state_count = @states.each_with_object(Hash.new(0)) {|state, hash| hash[state.name] += 1}   
+      activity_count = @activities.each_with_object(Hash.new(0)) {|activity, hash| hash[activity.name] += 1}
+    
+      @top_states = state_count.max(3) {|a, b| a[1] <=> b[1]}   # loop through state_count to collect top 3
+      @top_activities = activity_count.max(3) {|a, b| a[1] <=> b[1]}.collect {|a| a[0]}
 
-      # get all states for that user, not doubled up
-      @states = @user.adventures.collect {|a| a.states.uniq}.flatten
-      # get all activities for that user, not doubled up
-      @activities = @user.adventures.collect{|a| a.activities}.flatten
-      # count the instances of each one and find the max(3)
-      
       erb :"users/show"
     else
       redirect "/login"
@@ -35,3 +38,5 @@ class UserController < ApplicationController
     erb :"users/show_adventures"
   end
 end
+
+# @states.each_with_object(Hash.new(0)) {|state, hash| hash[state.name] += 1}     => found example of this on http://jerodsanto.net
