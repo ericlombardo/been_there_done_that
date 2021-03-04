1c010d756a9626484363427bbd97b7a58506d6c5
Eric <eric.m.lombardo@gmail.com>
2021-03-03 20:59:28 -0500

    Build state_filter logic and test

--- app/controllers/adventure_controller.rb
+++ app/controllers/adventure_controller.rb
@@ -2,7 +2,11 @@ class AdventureController < ApplicationController
 
   
   get "/adventures" do  # get route to show erb :index
+    binding.pry
     get_adventures
+    # if adventures empty 
+    # redirect to log adventure
+    # give flash message that no adventures yet, track to be first
     get_states
     get_activities
     erb :"adventures/index"
@@ -67,10 +71,11 @@ class AdventureController < ApplicationController
   end
 
   post "/adventures/state_filter" do
-
+    get_activities
+    get_states
     state_ids = AdventureStateActivity.where(state_id: params[:state_filter]).pluck(:adventure_id).uniq
     @state_filter = Adventure.find(state_ids)
-
+    # binding.pry
     erb :"adventures/index"
 
   end
