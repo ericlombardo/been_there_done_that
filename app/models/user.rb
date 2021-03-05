e878763144ed4e58b6bc9722b4659869da7a3f9f
Eric <eric.m.lombardo@gmail.com>
2021-03-05 14:38:19 -0500

    Add dependent destroy for deleting user account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :adventures
+  has_many :adventures, dependent: :destroy
   has_many :activities, through: :adventures
   has_many :states, through: :adventures
 
