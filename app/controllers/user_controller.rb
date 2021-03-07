340ad41daac0b5b79fb7c7e9aafec0afb0310fc3
Eric <eric.m.lombardo@gmail.com>
2021-03-07 06:44:10 -0500

    Refactor show page to slim down logic in erb file

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,16 +3,18 @@ class UserController < ApplicationController
   get "/users/:slug" do
     block_if_logged_out   #logged_in? helper method
     find_user
-    
-    @states = @user.adventures.collect {|a| a.states.uniq}.flatten    # get all states for that user, not doubled up
-    @activities = @user.adventures.collect{|a| a.activities}.flatten  # get all activities for that user
-  
-    # loops through states, create new hash set value to (0) if no value, create key/value for state.name/count
-    state_count = @states.each_with_object(Hash.new(0)) {|state, hash| hash[state.name] += 1}   
-    activity_count = @activities.each_with_object(Hash.new(0)) {|activity, hash| hash[activity.name] += 1}
-  
+
+    @state_list = []
+    states = @user.adventures.collect {|a| a.states.uniq}.flatten    # get all states for that user, no join table duplicates
+    state_count = states.each_with_object(Hash.new(0)) {|state, hash| hash[state.name] += 1}   # loops through states, create new hash set value to (0) if no value, create key/value for state.name/count
     @top_states = state_count.max(3) {|a, b| a[1] <=> b[1]}   # loop through state_count to collect top 3
-    @top_activities = activity_count.max(3) {|a, b| a[1] <=> b[1]}
+    states.uniq.each {|s| @state_list << s.name} # once total count is finished collect uniq states in state_list for display
+
+    @activity_list = []
+    activities = @user.adventures.collect{|a| a.activities}.flatten  # get all activities for that user
+    activity_count = activities.each_with_object(Hash.new(0)) {|activity, hash| hash[activity.name] += 1} # create key value pair with count for each activity
+    @top_activities = activity_count.max(3) {|a, b| a[1] <=> b[1]} # collec the top three activities
+    activities.uniq.each {|a| @activity_list << a.name}
 
     erb :"users/show"
   end
