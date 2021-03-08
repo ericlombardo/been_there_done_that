b5417bbb178946c0d22546b7e3676f0461d98896
Eric <eric.m.lombardo@gmail.com>
2021-03-07 20:24:26 -0500

    Add admin to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,51 +4,32 @@ activities = [
 	"Star Gazing",
 	"Hiking",
 	"Canoeing",
-	"Picknick",
 	"Rock Climbing",
 	"Cycling",
 	"Snow Skiing",
 	"Paddle Boarding",
 	"Fishing",
-	"Running",
 	"Trail Running",
 	"Mountain Biking",
 	"Water Skiing",
-	"Yoga",
-	"Whale Watching",
 	"Surfing",
 	"Snowboarding",
-	"Mudding",
 	"Nature Walk",
-	"Watch Sunset",
-	"Watch Surise",
-	"Sailing",
+	"Boating",
 	"Backpacking",
 	"Kayaking",
 	"Ultimate Frisbee",
-	"Beach",
-	"Creeking",
 	"Horseback Riding",
-	"Conservation Voluteering",
-	"Motorboating",
 	"Rafting",
 	"Skydiving",
 	"Ziplining",
-	"Bridge Climbing",
 	"Sandboarding",
 	"Bungy Jumping",
 	"Rappelling",
-	"Caving",
-	"Swimming with Sharks",
 	"Paragliding",
 	"Hang Gliding",
-	"Sky Swinging",
 	"Parasailing",
-	"ATV Riding",
-	"Glacier Climbing",
-	"Ice Fishing",
-  "Dogsledding",
-  "Nature Photography"]
+	"ATV Riding"]
 states = [{name: "Alabama", capital: "Montgomery", land_area: "50,744", state_forests: "21", state_parks: "22"}, {name: "Alaska", capital: "Juneau", land_area: "570,641", state_forests: "", state_parks: "100+"}, {name: "Arizona", capital: "Phoenix", land_area: "113,595", state_forests: "", state_parks: "28"}, {name: "Arkansas", capital: "Little Rock", land_area: "52,068", state_forests: "", state_parks: "51"}, {name: "California", capital: "Sacramento", land_area: "155,959", state_forests: "", state_parks: "278"}, {name: "Colorado", capital: "Denver", land_area: "103,717", state_forests: "1", state_parks: "44"}, {name: "Connecticut", capital: "Hartford", land_area: "4,844", state_forests: "94", state_parks: "94"}, {name: "Delaware", capital: "Dover", land_area: "1,954", state_forests: "3", state_parks: "14"}, {name: "Florida", capital: "Tallahassee", land_area: "53,927", state_forests: "31", state_parks: "159"}, {name: "Georgia", capital: "Atlanta", land_area: "57,906", state_forests: "6", state_parks: "64"}, {name: "Hawaii", capital: "Honolulu", land_area: "6,423", state_forests: "19", state_parks: "52"}, {name: "Idaho", capital: "Boise", land_area: "82,747", state_forests: "", state_parks: "30"}, {name: "Illinois", capital: "Springfield", land_area: "55,584", state_forests: "", state_parks: ""}, {name: "Indiana", capital: "Indianapolis", land_area: "35,867", state_forests: "", state_parks: "23"}, {name: "Iowa", capital: "Des Moines", land_area: "55,869", state_forests: "10", state_parks: "84"}, {name: "Kansas", capital: "Topeka", land_area: "81,815", state_forests: "", state_parks: "24"}, {name: "Kentucky", capital: "Frankfort", land_area: "39,728", state_forests: "5", state_parks: "52"}, {name: "Louisiana", capital: "Baton Rouge", land_area: "43,156", state_forests: "1", state_parks: "35"}, {name: "Maine", capital: "Augusta", land_area: "30,862", state_forests: "1", state_parks: "30+"}, {name: "Maryland", capital: "Annapolis", land_area: "9,774", state_forests: "7", state_parks: "40"}, {name: "Massachusetts", capital: "Boston", land_area: "7,840", state_forests: "", state_parks: ""}, {name: "Michigan", capital: "Lansing", land_area: "56,804", state_forests: "", state_parks: "97"}, {name: "Minnesota", capital: "St. Paul", land_area: "79,610", state_forests: "58", state_parks: "72"}, {name: "Mississippi", capital: "Jackson", land_area: "46,907", state_forests: "", state_parks: "24"}, {name: "Missouri", capital: "Jefferson City", land_area: "68,886", state_forests: "", state_parks: "81"}, {name: "Montana", capital: "Helena", land_area: "145,552", state_forests: "7", state_parks: "50"}, {name: "Nebraska", capital: "Lincoln", land_area: "77,358", state_forests: "", state_parks: "87"}, {name: "Nevada", capital: "Carson City", land_area: "110,577", state_forests: "", state_parks: "24"}, {name: "New Hampshire", capital: "Concord", land_area: "9,349", state_forests: "", state_parks: "72"}, {name: "New Jersey", capital: "Trenton", land_area: "8,722.58", state_forests: "11", state_parks: "42"}, {name: "New Mexico", capital: "Santa Fe", land_area: "121,589", state_forests: "", state_parks: "31"}, {name: "New York", capital: "Albany", land_area: "54,555", state_forests: "", state_parks: "176"}, {name: "North Carolina", capital: "Raleigh", land_area: "53,819", state_forests: "", state_parks: "29"}, {name: "North Dakota", capital: "Bismarck", land_area: "70,698", state_forests: "", state_parks: "17"}, {name: "Ohio", capital: "Columbus", land_area: "44,825", state_forests: "20", state_parks: "74"}, {name: "Oklahoma", capital: "Oklahoma City", land_area: "69,897", state_forests: "", state_parks: "50"}, {name: "Oregon", capital: "Salem", land_area: "98,381", state_forests: "", state_parks: "231"}, {name: "Pennsylvania", capital: "Harrisburg", land_area: "46,055", state_forests: "", state_parks: "116"}, {name: "Rhode Island", capital: "Providence", land_area: "1,214", state_forests: "", state_parks: "14"}, {name: "South Carolina", capital: "Columbia", land_area: "30,109", state_forests: "4", state_parks: "47"}, {name: "South Dakota", capital: "Pierre", land_area: "78,116", state_forests: "", state_parks: "12"}, {name: "Tennessee", capital: "Nashville", land_area: "41,217", state_forests: "15", state_parks: "54"}, {name: "Texas", capital: "Austin", land_area: "268,581", state_forests: "5", state_parks: "115"}, {name: "Utah", capital: "Salt Lake City", land_area: "84,899", state_forests: "40", state_parks: "40"}, {name: "Vermont", capital: "Montpelier", land_area: "9,616", state_forests: "", state_parks: "52"}, {name: "Virginia", capital: "Richmond", land_area: "42,774.2", state_forests: "16", state_parks: "34"}, {name: "Washington", capital: "Olympia", land_area: "71,362", state_forests: "", state_parks: "120"}, {name: "West Virginia", capital: "Charleston", land_area: "24,230", state_forests: "9", state_parks: "37"}, {name: "Wisconsin", capital: "Madison", land_area: "65,498.37", state_forests: "95", state_parks: "95"}, {name: "Wyoming", capital: "Cheyenne", land_area: "97,914", state_forests: "", state_parks: "24"}]
 titles = ["The Great Flood", "Hiking in Smokies", "The Great Snake Attack", "White Water Rafting", "Hiking at Hocking Hills", "Kayacking in Ocean", "Beach Vacation", "Paragliding In Smokies", "Skydying in Arizona", "Visiting the Grand Canyon", "Trip to Niagara Falls", "Hiking in Yosemite", "Visited Crazy Horse", "Buffalo Sitting in Wyoming", "Trip to Galcier National Park", "Hiked the Grand Tetons", "Drove to Zion National Park", "Swam in Mountain River", "Biked in West Virginia", "Backpacked the AT", "Backpacked PCT", "Backpacked CDT", "Backpacked around Lake Tahoe", "Camped in Yellowstone", "Camped in Tennessee", "Camped in South Carolina", "Maiden Voyage", "Skiing in Aspen", "Camped in Sedona", "Saw Old Faithfull", "Drove Stinking Creek Road", "Visited National Park", "Visited Acadia National Park", "Visited Arches National Park", "Visited Badlands National Park", "Visited Big Ben National Park", "Visited Bryce Canyon National Park", "Visited Capitol Reef National Park", "Visited Crater Lake National Park", "Visited Death Valley National Park", "Visited Denali National Park", "Visited Glacier National Park", "Visited Great Sand Dunes National Park", "Visited Isle Royale National Park", "Swam with the Dolphins", "Went to Honolulu", "Mountain Biked in Arizona", "Visted Friends in Montah", "Fished in Maine", "White Water Rafted In Red River Gorge"]
 weather = ["Rained the whole Time", "Mostly sunny in the 70's", "Great weather", "Not great but didn't effect trip", "A little on the cold side with 40's", "Perfect weather", "Stormed but we were prepared", "Not too bad with a fire", "Nice breeze the whole time", "Almost snowed in", "Cloudy but no rain", "Tornado Warning first day but cleared up", "Sunny and 75 degrees the whole", "Below freezing the whole time", "Windy but sunny and comfortable"]
@@ -65,7 +46,41 @@ states.each {|params| State.create(params)}
 activities.each {|activity| Activity.create(name: activity)}
 
 # Create admin account for testing
-User.create(username: "fj", email: "fj@fj.com", password: "asdfasdf")
+admin = User.create(username: "eric", email: "eric@wander.com", password: "codingisawesome")
+10.times do
+	adventure = Adventure.new
+	adventure.title = titles.sample
+	adventure.rating = Faker::Number.within(range: 1..10)
+	adventure.recommend = Faker::Boolean.boolean
+	adventure.start_date = Faker::Date.backward(days: 4000)
+	adventure.end_date = Faker::Date.between(from: adventure.start_date, to: Faker::Date.forward(days: 30))
+	adventure.miles_covered = Faker::Number.within(range: 50..3000)
+	adventure.companions = companions.sample(rand(0..4)).join(', ')
+	adventure.highlight = memories.sample
+	adventure.weather = weather.sample
+	adventure.summary = summaries.sample
+	adventure.transportation = Faker::Vehicle.make
+	adventure.food = foods.sample(rand(3..6)).join(', ')
+	adventure.private_notes = notes.sample
+	adventure.user_id = admin.id
+	
+
+	# saves adventures with all associations
+	adventure.save
+	# Create states and activities and link in AdventureStateActivity join table
+	states = State.all.sample(rand(1..3))
+	states.each.with_index(1) do |s, i| # loops through each one with index
+		activities = Activity.all.sample(rand(0..3))
+		activities.each do |a| # loops through each activity for that state
+			adventure.adventure_state_activities.create(state_id: s.id, activity_id: a.id) # creates instance for each using activity and adventure ids
+		end
+	end
+end	
+
+
+
+
+
 
 # Create User
 10.times do
@@ -84,18 +99,17 @@ User.create(username: "fj", email: "fj@fj.com", password: "asdfasdf")
     adventure.start_date = Faker::Date.backward(days: 4000)
     adventure.end_date = Faker::Date.between(from: adventure.start_date, to: Faker::Date.forward(days: 30))
     adventure.miles_covered = Faker::Number.within(range: 50..3000)
-    adventure.companions = companions.sample(rand(0..4))
+		adventure.companions = companions.sample(rand(0..4)).join(', ')
     adventure.highlight = memories.sample
     adventure.weather = weather.sample
     adventure.summary = summaries.sample
     adventure.transportation = Faker::Vehicle.make
-    adventure.food = foods.sample(rand(3..6))
+    adventure.food = foods.sample(rand(3..6)).join(', ')
     adventure.private_notes = notes.sample
 		adventure.user_id = user.id
 
 		# saves adventures with all associations
 		adventure.save
-		
 		# Create states and activities and link in AdventureStateActivity join table
 		states = State.all.sample(rand(1..3))
 		states.each.with_index(1) do |s, i| # loops through each one with index
@@ -104,6 +118,5 @@ User.create(username: "fj", email: "fj@fj.com", password: "asdfasdf")
 				adventure.adventure_state_activities.create(state_id: s.id, activity_id: a.id) # creates instance for each using activity and adventure ids
 			end
 		end
-
   end
 end
